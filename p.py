a31fc90d5c3dced30ffa060e51fecd7580af49dc
nicoprodi <47774149+nicoprodi@users.noreply.github.com>
2021-12-14 14:13:26 +0200

    Update p.py

--- p.py
+++ p.py
@@ -1,31 +1,4 @@
 
-def main():
-    file1=input("File 1 -- ")
-    file2=input("File 2 -- ")
-
-    try:
-        f = open(file1, "r")
-    except IOError:
-        print("File 1 not accessible")
-        return
-    x = f.read()                        # x contains the strings from first file
-
-    try:
-        f2 = open(file2, "r")
-    except IOError:
-        print("File 2 not accessible")
-        return
-    y = f2.read()                         # y contains the strings from first file
-
-    x = dots(x)
-    y = dots(y)
-
-    sentence = x.split(".")
-    sentence2 = y.split(".")
-
-    check(sentence, sentence2)
-
-
 def dots(x):
     x = x.replace('?', '.')
     x = x.replace('!', '.')
@@ -50,5 +23,37 @@ def checkWords(sentence, sentence2):
     if sentence == sentence2:
         return 1
     return 0
+
+def main2():
+    file1=input("File 1 -- ")
+    file2=input("File 2 -- ")
+
+    try:
+        f = open(file1, "r")
+    except IOError:
+        print("File 1 not accessible")
+        return
+    x = f.readline()                        # x contains the strings from first file
+
+    try:
+        f2 = open(file2, "r")
+    except IOError:
+        print("File 2 not accessible")
+        return
+
+    y = f2.readline()
+    while y:                            # y contains the strings from first file
+        y = dots(y)
+        sentence2 = y.split(".")
+        f = open(file1, "r")
+        x = f.readline()
+        while x:
+            x = dots(x)
+            sentence = x.split(".")
+            check(sentence, sentence2)
+            x = f.readline()
+        f.close()
+        y = f2.readline()
+
 
 main()
